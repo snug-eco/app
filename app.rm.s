47ec39ec61d62088b3a294fc0f0b3141dff02eeb
S1mon <jaforsi.kozak@gmail.com>
2026-01-10 13:15:00 +0100

    fixed rm

--- app.rm.s
+++ app.rm.s
@@ -2,54 +2,42 @@ jsr main
 brk
 
 
-use lib.quad.s
+use lib.sys.s
 use lib.mem.s
-use lib.heap.s
 use lib.string.s
 use lib.args.s
 
-var _iter
 var _name
 
 lab main
-    lit 4
-    jsr heap/new
-    stv _iter
-
     ; file name
     jsr args/get
     stv _name
 
     ; check arg provided
     ldv _name
-    lit 0
-    equ
+    not
     jcn no-file-error
 
     ; check file exists
     ldv _name
-    s04 ; fs_check
-    lit 0
-    equ
+    jsr sys/file/check
+    not
     jcn file-not-exist-error
 
     ; seek file
-    ldv _iter
     ldv _name
-    s05 ; fs_seek
-
-    ldv _iter
-    s09 ; fs_delete
+    jsr sys/file/seek
+    jsr sys/file/delete
 
     brk
 
 
 lab file-not-exist-error
-    lit 80
-    jsr heap/new
+    lit 100
+    jsr sys/heap/alloc
     dup
     str "rm error: no such file "
-    dup
     jsr string/print
     ldv _name
     jsr string/print
@@ -57,9 +45,10 @@ lab file-not-exist-error
     brk
 
 lab no-file-error
-    lit 0 
+    lit 100
+    jsr sys/heap/alloc
+    dup
     str "rm error: no file name provided"
-    lit 0
     jsr string/print
     jsr string/newline
     brk
